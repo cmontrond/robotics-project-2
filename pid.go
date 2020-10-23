e83898e621dba5e8dab9f217aeec35f7d25edf82
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-10-23 19:05:13 -0400

    removed unused code

--- pid.go
+++ pid.go
@@ -1,44 +1,104 @@
 package main
 
-type Pid struct {
-	Kp            float64
-	Ki            float64
-	Kd            float64
-	Derivator     float64
-	Integrator    float64
-	IntegratorMax float64
-	IntegratorMin float64
-	SetPoint      float64
-	Error         float64
-	Pvalue        float64
-	Dvalue        float64
-	Ivalue        float64
-	Pid           float64
+import (
+	"errors"
+)
+
+type PID struct {
+	iTerm, lastInput, minOutput, maxOutput, kp, ki, kd float64
+	sampleTime                                         int
+	limitsActive                                       bool
 }
 
-func (pid *Pid) Update(currentValue float64) {
-
-	pid.Error = pid.SetPoint - currentValue
-
-	pid.Pvalue = pid.Kp * pid.Error
-	pid.Dvalue = pid.Kd * (pid.Error - pid.Derivator)
-	pid.Derivator = pid.Error
-
-	pid.Integrator = pid.Integrator + pid.Error
-
-	if pid.Integrator > pid.IntegratorMax {
-		pid.Integrator = pid.IntegratorMax
-	} else if pid.Integrator < pid.IntegratorMin {
-		pid.Integrator = pid.IntegratorMin
+func NewPID(interval int, kp, ki, kd float64) *PID {
+	return &PID{
+		sampleTime: interval,
+		kp:         kp,
+		ki:         ki,
+		kd:         kd,
 	}
-
-	pid.Ivalue = pid.Integrator * pid.Ki
-
-	pid.Pid = pid.Pvalue + pid.Ivalue + pid.Dvalue
 }
 
-func (pid *Pid) SetPointValue(setPoint float64) {
-	pid.SetPoint = setPoint
-	pid.Integrator = 0
-	pid.Derivator = 0
+func (pid *PID) SetOutputLimits(min, max float64) error {
+	if min >= max {
+		return errors.New("minimum should be smaller than maximum")
+	}
+	pid.limitsActive = true
+	pid.minOutput = min
+	pid.maxOutput = max
+	return nil
 }
+
+func (pid *PID) SetTunings(kp, ki, kd float64) error {
+	if kp < 0 || ki < 0 || kd < 0 {
+		return errors.New("tuning parameters must be greater than 0")
+	}
+	pid.kp = kp
+	pid.ki = ki * float64(pid.sampleTime)
+	pid.kd = ki / float64(pid.sampleTime)
+	return nil
+}
+
+func (pid *PID) SetSampleTime(seconds int) error {
+	if seconds < 1 {
+		return errors.New("the interval must be > 0 seconds")
+	}
+	var ratio float64
+	if pid.sampleTime > 0 {
+		ratio = float64(seconds) / float64(pid.sampleTime)
+	} else {
+		ratio = 1
+	}
+	pid.ki = pid.ki * ratio
+	pid.kd = pid.kd / ratio
+	pid.sampleTime = seconds
+	return nil
+}
+
+func (pid *PID) Compute(setPoint, input float64) float64 {
+	err := setPoint - input
+	pid.iTerm = pid.ki * err
+	if pid.limitsActive {
+		if pid.iTerm > pid.maxOutput {
+			pid.iTerm = pid.maxOutput
+		} else if pid.iTerm < pid.minOutput {
+			pid.iTerm = pid.minOutput
+		}
+	}
+	dInput := input - pid.lastInput
+	output := pid.kp*err + pid.ki*pid.iTerm + pid.kd*dInput
+	if pid.limitsActive {
+		if output > pid.maxOutput {
+			output = pid.maxOutput
+		} else if output < pid.minOutput {
+			output = pid.minOutput
+		}
+	}
+	pid.lastInput = 0.0
+	return output
+}
+
+func (pid *PID) Reset() {
+	pid.iTerm = 0.0
+	pid.lastInput = 0
+}
+
+//func main() {
+//	inputs := []float64{34.0, 100.0, 600.0, 20.0, 90.0, 60.0}
+//	setPoint := 60.0
+//	pid := NewPID(1, 1.0, 1.0, 0.0)
+//	err := pid.SetSampleTime(1)
+//
+//	if err != nil {
+//		println("Error")
+//	}
+//
+//	for i, value := range inputs {
+//		println("Index: ", i)
+//		fmt.Printf("SetPoint: %.2f\n", setPoint)
+//		fmt.Printf("Input: %.2f\n", value)
+//		fmt.Printf("LastInput: %.2f\n", pid.lastInput)
+//		fmt.Printf("Output: %.2f\n", pid.Compute(setPoint, value))
+//		fmt.Printf("\n\n")
+//	}
+//}
