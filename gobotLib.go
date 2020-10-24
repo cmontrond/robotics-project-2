499eef03796cda462fd1d5a9984fafece058f577
Christopher Montrond <cmontronddaveigafern@student.bridgew.edu>
2020-10-23 22:06:20 -0400

    move util functions to separate file clean code in main file

---
+++ gobotLib.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	g "gobot.io/x/gobot/platforms/dexter/gopigo3"
+	"time"
+)
+
+func Forward(gopigo3 *g.Driver, speed int) {
+	err := gopigo3.SetMotorDps(g.MOTOR_LEFT+g.MOTOR_RIGHT, speed)
+	if err != nil {
+		fmt.Errorf("Error moving the robot forward: %+v", err)
+	}
+}
+
+func Left(gopigo3 *g.Driver, speed int) {
+	err := gopigo3.SetMotorDps(g.MOTOR_LEFT, 0)
+	if err != nil {
+		fmt.Errorf("Error moving the robot backward: %+v", err)
+	}
+	err = gopigo3.SetMotorDps(g.MOTOR_RIGHT, speed)
+	if err != nil {
+		fmt.Errorf("Error moving the robot backward: %+v", err)
+	}
+}
+
+func Right(gopigo3 *g.Driver, speed int) {
+	err := gopigo3.SetMotorDps(g.MOTOR_LEFT, speed)
+	if err != nil {
+		fmt.Errorf("Error moving the robot backward: %+v", err)
+	}
+	err = gopigo3.SetMotorDps(g.MOTOR_RIGHT, 0)
+	if err != nil {
+		fmt.Errorf("Error moving the robot backward: %+v", err)
+	}
+}
+
+func SpinRight(gopigo3 *g.Driver, speed int) {
+	err := gopigo3.SetMotorDps(g.MOTOR_LEFT, speed)
+	if err != nil {
+		fmt.Errorf("Error moving the robot backward: %+v", err)
+	}
+	err = gopigo3.SetMotorDps(g.MOTOR_RIGHT, speed*-1)
+	if err != nil {
+		fmt.Errorf("Error moving the robot backward: %+v", err)
+	}
+}
+
+func ReadEnconders(gopigo3 *g.Driver) (int64, int64) {
+	leftEncoder, err := gopigo3.GetMotorEncoder(g.MOTOR_LEFT)
+
+	if err != nil {
+		fmt.Errorf("Error reading left encoder: %+v", err)
+	}
+
+	rightEncoder, err := gopigo3.GetMotorEncoder(g.MOTOR_RIGHT)
+
+	if err != nil {
+		fmt.Errorf("Error reading right encoder: %+v", err)
+	}
+
+	return leftEncoder, rightEncoder
+}
+
+func ReadEncodersAverage(gopigo3 *g.Driver, wheelCircumference float64) float64 {
+
+	left, right := ReadEnconders(gopigo3)
+
+	average := float64((left + right) / 2)
+
+	// We want the return value in centimetres, so that's why we do this
+	average = ((average / 360) * wheelCircumference) / 10
+
+	return average
+}
+
+func BlinkLED(gopigo3 *g.Driver) {
+	err := gopigo3.SetLED(g.LED_EYE_RIGHT, 0x00, 0x00, 0xFF)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	time.Sleep(time.Second)
+
+	err = gopigo3.SetLED(g.LED_EYE_RIGHT, 0x00, 0x00, 0x00)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	time.Sleep(time.Second)
+}
+
+func Stop(gopigo3 *g.Driver) {
+	err := gopigo3.SetMotorDps(g.MOTOR_LEFT+g.MOTOR_RIGHT, 0)
+	if err != nil {
+		fmt.Errorf("Error stopping the robot: %+v", err)
+	}
+}
--- secondProject.go
+++ secondProject.go
@@ -15,97 +15,6 @@ const (
 	SENSOR_SIZE = 4 // We use to discard extra cm in our final length calculation
 )
 
-func Forward(gopigo3 *g.Driver, speed int) {
-	err := gopigo3.SetMotorDps(g.MOTOR_LEFT+g.MOTOR_RIGHT, speed)
-	if err != nil {
-		fmt.Errorf("Error moving the robot forward: %+v", err)
-	}
-}
-
-func Left(gopigo3 *g.Driver, speed int) {
-	err := gopigo3.SetMotorDps(g.MOTOR_LEFT, 0)
-	if err != nil {
-		fmt.Errorf("Error moving the robot backward: %+v", err)
-	}
-	err = gopigo3.SetMotorDps(g.MOTOR_RIGHT, speed)
-	if err != nil {
-		fmt.Errorf("Error moving the robot backward: %+v", err)
-	}
-}
-
-func Right(gopigo3 *g.Driver, speed int) {
-	err := gopigo3.SetMotorDps(g.MOTOR_LEFT, speed)
-	if err != nil {
-		fmt.Errorf("Error moving the robot backward: %+v", err)
-	}
-	err = gopigo3.SetMotorDps(g.MOTOR_RIGHT, 0)
-	if err != nil {
-		fmt.Errorf("Error moving the robot backward: %+v", err)
-	}
-}
-
-func SpinRight(gopigo3 *g.Driver, speed int) {
-	err := gopigo3.SetMotorDps(g.MOTOR_LEFT, speed)
-	if err != nil {
-		fmt.Errorf("Error moving the robot backward: %+v", err)
-	}
-	err = gopigo3.SetMotorDps(g.MOTOR_RIGHT, speed*-1)
-	if err != nil {
-		fmt.Errorf("Error moving the robot backward: %+v", err)
-	}
-}
-
-func ReadEnconders(gopigo3 *g.Driver) (int64, int64) {
-	leftEncoder, err := gopigo3.GetMotorEncoder(g.MOTOR_LEFT)
-
-	if err != nil {
-		fmt.Errorf("Error reading left encoder: %+v", err)
-	}
-
-	rightEncoder, err := gopigo3.GetMotorEncoder(g.MOTOR_RIGHT)
-
-	if err != nil {
-		fmt.Errorf("Error reading right encoder: %+v", err)
-	}
-
-	return leftEncoder, rightEncoder
-}
-
-func ReadEncodersAverage(gopigo3 *g.Driver, wheelCircumference float64) float64 {
-
-	left, right := ReadEnconders(gopigo3)
-
-	average := float64((left + right) / 2)
-
-	// We want the return value in centimetres, so that's why we do this
-	average = ((average / 360) * wheelCircumference) / 10
-
-	return average
-}
-
-func BlinkLED(gopigo3 *g.Driver) {
-	err := gopigo3.SetLED(g.LED_EYE_RIGHT, 0x00, 0x00, 0xFF)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	time.Sleep(time.Second)
-
-	err = gopigo3.SetLED(g.LED_EYE_RIGHT, 0x00, 0x00, 0x00)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	time.Sleep(time.Second)
-}
-
-func Stop(gopigo3 *g.Driver) {
-	err := gopigo3.SetMotorDps(g.MOTOR_LEFT+g.MOTOR_RIGHT, 0)
-	if err != nil {
-		fmt.Errorf("Error stopping the robot: %+v", err)
-	}
-}
-
 func robotRunLoop(gopigo3 *g.Driver, lidarSensor *i2c.LIDARLiteDriver) {
 
 	pid := NewPID(1, 1.0, 1.0, 0.0)
@@ -335,7 +244,7 @@ func main() {
 		robotRunLoop(gopigo3, lidarSensor)
 	}
 
-	robot := gobot.NewRobot("Project 2",
+	robot := gobot.NewRobot("Project 2: Block Measurer",
 		[]gobot.Connection{raspiAdaptor},
 		[]gobot.Device{gopigo3, lidarSensor},
 		mainRobotFunc,
